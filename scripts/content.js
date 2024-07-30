865cbdf8d5be06a81c64dc33db4f404a95ef92a2
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-31 00:13:02 +0300

    inited

---
+++ scripts/content.js
@@ -0,0 +1,8 @@
+let clam = document.querySelector('body')?.textContent
+let wordies = text.split(' ')
+let newdev = document.createElement('div')
+Object.assign(newdev.style, {position :'fixed',
+    padding :'150px',border: '1px solid', backgroundColor:'#aaeeff',top:'15px',left:'15px'
+} )
+newdev.innerHTML='fibeb'
+document.querySelector('body')?.appendChild(newdev)
