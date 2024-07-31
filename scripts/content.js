8b5d05cad0d42f9ee28f3b6ee6683092db2cbe4f
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-31 17:09:39 +0300

    cleared

--- scripts/content.js
+++ scripts/content.js
@@ -1,35 +1,7 @@
-// let newdiv = document.createElement('div')
-// Object.assign(newdiv.style, {position :'fixed',
-//     padding :'150px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
-// } )
-// newdiv.id='newdiv'
-// document.body.appendChild(newdiv)
-// let cancel=document.createElement('button')
-// Object.assign(cancel.style, {position :'absolute',backgroundColor:'rgba(0,0,0,0)',top:0, right:0,border:0,padding:'7px',color:'black'})
-// cancel.id='canext'
-// cancel.textContent='x';
-// document.querySelector('#newdiv').appendChild(cancel)
-// document.querySelector('#canext').addEventListener('click',()=>{
-//     document.querySelector('#newdiv').style.display='none'
-
-// })
-// // newdiv.textContent='fngdfjgngng'
-// console.log('changed')
-let frame = document.createElement('div')
-// frame.id='newdiv'
-frame.innerHTML=` <div id="newdiv"
-    <button id="canext">x</button>
-    <textarea name="" id="txtplace"></textarea>
-    <div id="sply"></div>
-    </div>`
-
-document.body.appendChild(frame)
-console.log('changed')
-let newdiv = document.querySelector('#newdiv')
-let cancel = document.querySelector('#canext')
-let displayer = document.querySelector('#sply')
-let txtinter=document.querySelector('#txtplace')
-Object.assign(newdiv.style, {position :'fixed',
-        padding :'15px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
-    } );
-Object.assign(cancel.style, {position :'absolute',backgroundColor:'rgba(0,0,0,0)',top:0, right:0,border:0,padding:'7px',color:'black'})
+let full = document.createElement('div')
+Object.assign(full.style,{position:'fixed', background:'radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(0,212,255,0.33657212885154064) 100%)',padding:'150px',top:'15px',left:'15px'});
+full.textContent='jflkdgndfg'
+document.bodt.appendChild(full)
+document.body.onclick=()=>{
+    full.hidden = true;
+}
