988f27cd3bbc08514391897601fbd91d98ea2c51
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-31 15:27:29 +0300

    dif way

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,31 @@
-let newdiv = document.createElement('div')
-Object.assign(newdiv.style, {position :'fixed',
-    padding :'150px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
-} )
-newdiv.id='newdiv'
-document.body.appendChild(newdiv)
-let cancel=document.createElement('button')
-Object.assign(cancel.style, {position :'absolute',backgroundColor:'rgba(0,0,0,0)',top:0, right:0,border:0,padding:'7px',color:'black'})
-cancel.id='canext'
-cancel.textContent='x';
-document.querySelector('#newdiv').appendChild(cancel)
-document.querySelector('#canext').addEventListener('click',()=>{
-    document.querySelector('#newdiv').style.display='none'
+// let newdiv = document.createElement('div')
+// Object.assign(newdiv.style, {position :'fixed',
+//     padding :'150px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
+// } )
+// newdiv.id='newdiv'
+// document.body.appendChild(newdiv)
+// let cancel=document.createElement('button')
+// Object.assign(cancel.style, {position :'absolute',backgroundColor:'rgba(0,0,0,0)',top:0, right:0,border:0,padding:'7px',color:'black'})
+// cancel.id='canext'
+// cancel.textContent='x';
+// document.querySelector('#newdiv').appendChild(cancel)
+// document.querySelector('#canext').addEventListener('click',()=>{
+//     document.querySelector('#newdiv').style.display='none'
 
-})
-// newdiv.textContent='fngdfjgngng'
-console.log('changed')
+// })
+// // newdiv.textContent='fngdfjgngng'
+// console.log('changed')
+let frame = document.createElement('div')
+frame.innerHTML=` <div id="newdiv">
+    <button id="canext">x</button>
+    <textarea name="" id="txtplace"></textarea>
+    <div id="sply"></div>
+  </div>`
+
+document.body.appendChild(frame)
+let newdiv = dovument.querySelector('#newdiv')
+let cancel = dovument.querySelector('#canext')
+let displayer = document.querySelector('#sply')
+Object.assign(newdiv.style, {position :'fixed',
+        padding :'150px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
+    } )
