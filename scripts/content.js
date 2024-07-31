4935d9bc14ca9260d058af7c1ea20a7359e8868c
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-31 15:48:07 +0300

    style change

--- scripts/content.js
+++ scripts/content.js
@@ -16,17 +16,20 @@
 // // newdiv.textContent='fngdfjgngng'
 // console.log('changed')
 let frame = document.createElement('div')
-frame.id='newdiv'
-frame.innerHTML=` 
+// frame.id='newdiv'
+frame.innerHTML=` <div id="newdiv"
     <button id="canext">x</button>
     <textarea name="" id="txtplace"></textarea>
-    <div id="sply"></div>`
+    <div id="sply"></div>
+    </div>`
 
 document.body.appendChild(frame)
 console.log('changed')
 let newdiv = document.querySelector('#newdiv')
 let cancel = document.querySelector('#canext')
 let displayer = document.querySelector('#sply')
+let txtinter=document.querySelector('#txtplace')
 Object.assign(newdiv.style, {position :'fixed',
         padding :'15px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
-    } )
+    } );
+Object.assign(cancel.style, {position :'absolute',backgroundColor:'rgba(0,0,0,0)',top:0, right:0,border:0,padding:'7px',color:'black'})
