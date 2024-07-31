1d29a5e0ddc3cf32c00d341b2945b180bfc0286c
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-31 15:10:38 +0300

    nm

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,17 @@
-let clam = document.querySelector('body')?.textContent
-let wordies = text.split(' ')
-let newdev = document.createElement('div')
-Object.assign(newdev.style, {position :'fixed',
-    padding :'150px',border: '1px solid', backgroundColor:'#aaeeff',top:'15px',left:'15px'
+let newdiv = document.createElement('div')
+Object.assign(newdiv.style, {position :'fixed',
+    padding :'150px',border: '1px solid', backgroundColor:'#ddeeff',top:'15px',left:'15px','z-index':5000
 } )
-newdev.innerHTML='fibeb'
-document.querySelector('body')?.appendChild(newdev)
+newdiv.id='newdiv'
+document.body.appendChild(newdiv)
+let cancel=document.createElement('button')
+Object.assign(cancel.style, {position :'absolute',backgroundColor:'rgba(0,0,0,0)',top:0, right:0,border:0,padding:'7px',color:'black'})
+cancel.id='canext'
+cancel.textContent='x';
+document.querySelector('#canext').addEventListener('click',()=>{
+    document.querySelector('#newdiv').style.display='none'
+
+})
+document.querySelector('#newdiv').appendChild(cancel)
+// newdiv.textContent='fngdfjgngng'
+console.log('changed')
